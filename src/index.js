e0d5ca4689e4faa104ea6e7a4b1d8afa43fae64e
James Kraus <jamespkraus@gmail.com>
2018-04-05 20:10:51 -0400

    Create basic pie chart

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
+import * as d3 from 'd3'
+
+const data = [1, 2, 3, 10, 4, 5, 6]
+
+// An array of nice colors for the chart
+const colors = d3.schemeSet2
+
+// Creates a function that returns the path 'd' attribute
+// for some set of arc properties
+const arcGenerator = d3
+  .arc()
+  .outerRadius(4)
+  .innerRadius(0)
+  .startAngle(0)
+
+// Create an array of arc properties
+const pieArcs = d3.pie()(data)
+
+// Create the 'd' attrbutes from the arc properties
+const arcPaths = pieArcs.map(arcGenerator)
 
 const App = () => (
-  <div>
-    <h1>Hello World</h1>
-    <p>This is a starter template</p>
-  </div>
+  <svg viewBox="0 0 10 10">
+    <g transform="translate(5, 5)">
+      {arcPaths.map((arc, i) => <path d={arc} fill={colors[i]} />)}
+    </g>
+  </svg>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
